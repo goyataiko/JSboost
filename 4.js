98d2eb2ed5f3d63729528fc7da86be3783aa9968
Takano Computer <goyataiko@gmail.com>
2023-07-12 13:50:17 +0900

    04 task

---
+++ 4.js
@@ -0,0 +1,11 @@
+let mon = 2;
+if (mon == 1) {
+    console.log('January'); 
+} else if(mon == 2) {
+    console.log('February'); 
+} else if(mon == 3) {
+    console.log('March'); 
+} else {
+    console.log('不明です'); 
+}
+
